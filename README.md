410579b53c54075b9dff0f1fe25a150b1cd109cd
HelChris <111705707+HelChris@users.noreply.github.com>
2023-10-28 22:30:43 +0200

    update

--- README.md
+++ README.md
@@ -3,17 +3,13 @@
 # Programming Foundations CA
 
 Course Assignment in programming Foundations.
-Link to live site:
-https://bookshelftesting.netlify.app/
-![Screenshot of the live page](/images/screenshot.png)
+![Screenshot of the page](./images/screenshot.png)
 
----
-
-In this CA I've done my best to make use of the basics within the programming language Javascript to describe an everyday object. The everyday object I've chosen is a bookshelf with books on the shelfs. In the process I've learnt to use the following property types:
+In this CA I've done my best to make use of the basics within the programming language Javascript to describe an everyday object. The everyday object I've chosen is a bookshelf with "books" on the shelfs. In the process I've learnt to use the following property types:
 
 - String -> "used to declare author, material, publisher etc."
 - Number -> used to declare dimensions, pages, publication year etc.
-- Boolean -> the true/false used to state that the bookshelf is assembled and that the bookshelf indeed contain a number of books.
+- Boolean -> the true/false used to state that the bookshelf is assembled, that the bookshelf indeed contains a number of books, if the book is on the shelf or not.
 - Array -> the book objects stated in the code is my array of objects.
 - Function -> added functions to add or remove books, get the total number of books and retrive the price of all the books.
 
@@ -39,11 +35,15 @@ books stored on the shelf:
 six books: books 1-6.
 
 --------The Books--------
-There are a total of six books, they all have these properties: title, author, published (date), publisher, genre, pages, prive, currency, available, language.
-The value of these properties varies from book to book, but they all share the same author, currency, language and availability.
+There are a total of six books, they all have these properties: title, author, published (date), publisher, genre, pages, price, currency, onShelf, language.
+The value of these properties varies from book to book, but they all share the same author, currency and language.
 
 ---
 
 Additional features:
 
-I've added some CSS and HTML to create a visual of the bookshelf stated in the JS.
+I've added some CSS and HTML to create a visual of the bookshelf stated in the JS,
+added script in HTML to retrive some of the information about the books to the "bookshelf",
+used ID to retrive the list of the books.
+
+---
--- js/script.js
+++ js/script.js
@@ -1,161 +1,8 @@
-/**
- * Practice: Building objects
- *
- * - Create JavaScript objects based on objects in your current environment.
- * - Give each object an identifiable name.
- * - Create properties to describe the objects and set their values.
- * - Find an object that has another object inside of it to create a nested object.
- * - Test your objects in the browser console by accessing the entire object and its specific properties.
-
- * The item must have at least one property of each of the following `type`s:
-
-    1. String
-    2. Number
-    3. Boolean
-    4. Array
-    5. Function
-
-
-const book1 = {
-    title: "The Good Book",
-    author: "Tim Minchin",
-    published: 2009,
-    publisher: "Ready Books",
-    genre: "History",
-    pages: 200,
-    price: 200,
-    currency: "NOK",
-    available: true,
-    language: "english",
-// Adding a method to display book information in a formatted manner
-    getBookInfo: function() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
-    }
-}
-
-const book2 = {
-    title: "Rock 'n' Roll Nerd",
-    author: "Tim Minchin",
-    published: 2005,
-    publisher: "Darkside Books",
-    genre: "Memoir",
-    pages: 777,
-    price: 300,
-    currency: "NOK",
-    available: true,
-    language: "english",
-// Adding a method to display book information in a formatted manner
-    getBookInfo: function() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
-    }
-}
-
-const book3 = {
-    title: "Not Perfect",
-    author: "Tim Minchin",
-    published: 2005,
-    publisher: "Darkside Books",
-    genre: "Philosophical",
-    pages: 666,
-    price: 450,
-    currency: "NOK",
-    available: true,
-    language: "english",
-// Adding a method to display book information in a formatted manner
-    getBookInfo: function() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
-    }
-}
-
-const book4 = {
-    title: "If I Didn't Have You",
-    author: "Tim Minchin",
-    published: 2009,
-    publisher: "Ready Books",
-    genre: "Biography",
-    pages: 555,
-    price: 350,
-    currency: "NOK",
-    available: true,
-    language: "english",
-// Adding a method to display book information in a formatted manner
-    getBookInfo: function() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
-    }
-}
-
-const book5 = {
-    title: "Inflatable You",
-    author: "Tim Minchin",
-    published: 2005,
-    publisher: "Darkside Books",
-    genre: "Self-help",
-    pages: 444,
-    price: 150,
-    currency: "NOK",
-    available: true,
-    language: "english",
-// Adding a method to display book information in a formatted manner
-    getBookInfo: function() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
-    }
-}
-
-const book6 = {
-    title: "You Grew On Me",
-    author: "Tim Minchin",
-    published: 2006,
-    publisher: "Rock Books",
-    genre: "True Crime",
-    pages: 333,
-    price: 250,
-    currency: "NOK",
-    available: true,
-    language: "english",
-// Adding a method to display book information in a formatted manner
-    getBookInfo: function() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
-    }
-}
-
-const shelf = {
-    //material properties
-    material: "wood",
-    color: "brown",
-
-    //Dimensions
-    unit: "cm",
-    width: 200,
-    height: 150,
-    depth: 60,
-
-    //Configuration
-    numberOfShelves: 4, //specifies the number of individual shelves
-    assembled: true, //indicates if the shelf is already assembled
-
-    //Books stored on the shelf
-    books: [book1, book2, book3, book4, book5, book6], // Initially, the shelf has six books
-
-    //Method to add a book to the shelf
-    addBook: function(book) {
-        this.books.push(book);
-    },
-
-    //Method to remove a book from the shelf by title
-    removeBook: function(title) {
-        this.books = this.books.filter(book => book.title !== title);
-    }
-};
-
-*/
-
-// -----------------------------------
-
 // Book constructor
 class Book {
     static idCounter = 0; // static counter shared by all Book instances
 
-    constructor(title, author, published, publisher, genre, pages, price, currency, available, language) {
+    constructor(title, author, published, publisher, genre, pages, price, currency, onShelf, language) {
         Book.idCounter++; //increment the counter for each new book
         this.id = Book.idCounter; // assign the current value of counter to this book's ID
 
@@ -167,21 +14,21 @@ class Book {
         this.pages = pages;
         this.price = price;
         this.currency = currency;
-        this.available = available;
+        this.onShelf = onShelf;
         this.language = language;
     }
     //Adding a method to display book information in a formatted manner
     getBookInfo() {
-        return `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price} ${this.currency}`;
+        return `Title: ${this.title}\nAuthor: ${this.author}\nPublished: ${this.published}\nPublisher: ${this.publisher}\nPrice: ${this.price} ${this.currency}\nOnShelf: ${this.onShelf} `;
     }
 }
 
 //create book instances
-const book1 = new Book("The Good Book", "Tim Minchin", 2009, "Ready Books", "History", 300, 200, "NOK", true, "english");
+const book1 = new Book("The Good Book", "Tim Minchin", 2009, "Ready Books", "History", 300, 200, "NOK", false, "english");
 const book2 = new Book("Rock n Roll Nerd", "Tim Minchin", 2005, "Darkside Books", "Memoir", 777, 300, "NOK", true, "english");
 const book3 = new Book("Not Perfect", "Tim Minchin", 2005, "Darkside Books", "Philosophical", 666, 450, "NOK", true, "english");
 const book4 = new Book("If I Didn't Have You", "Tim Minchin", 2009, "Ready Books", "Biography", 555, 350, "NOK", true, "english");
-const book5 = new Book("Inflatable You", "Tim Minchin", 2005, "Darkside Books", "Self-help", 444, 150, "NOK", true, "english");
+const book5 = new Book("Inflatable You", "Tim Minchin", 2005, "Darkside Books", "Self-help", 444, 150, "NOK", false, "english");
 const book6 = new Book("You Grew On Me", "Tim Minchin", 2006, "Rock Books", "True Crime", 333, 250, "NOK", true, "english");
 
 const shelf = {
@@ -189,18 +36,18 @@ const shelf = {
     material: "wood",
     color: "brown",
 
-    //Dimensions
+    //dimensions
     unit: "cm",
     width: 200,
     height: 150,
     depth: 60,
 
-    //Configuration
+    //donfiguration
     numberOfShelves: 4,
     assembled: true,
     empty: false,
 
-    //Books stored on the shelf
+    //books stored on the shelf
     books: [book1, book2, book3, book4, book5, book6],
 
     addBook: function(book) {
